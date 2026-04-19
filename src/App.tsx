524ebcf1ba67b7ee26fb2bfc4be7caecb426a9a5
El John <bongaeljohn07@gmail.com>
2026-04-19 22:46:52 +0800

    setup base styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 export function App() {
-  return <div className="my-2 bg-gray-300 px-4 text-sm font-bold">Hello World</div>;
+  return <div className="my-2 bg-gray-300 px-4 text-sm">Hello World</div>;
 }
