da930574579a6195c7f00e502456a2f440cd4dfa
El John <bongaeljohn07@gmail.com>
2026-05-11 22:17:40 +0800

    implement update user password

--- src/features/settings/api/index.ts
+++ src/features/settings/api/index.ts
@@ -1,8 +1,30 @@
+import { isAxiosError } from 'axios';
 import { api } from '@/api';
 import type { User } from '@/features/auth/types';
 import type { UpdateDetailsPayload } from '@/features/settings/components/update-profile-details-form';
+import type { UpdatePasswordPayload } from '@/features/settings/components/update-password-form';
 
 export const updateCurrentUser = async (payload: UpdateDetailsPayload) => {
   const { data } = await api.patch<User>('/users/current-user', payload);
   return data;
+};
+
+export const updateCurrentUserPassword = async (payload: UpdatePasswordPayload) => {
+  try {
+    const { data } = await api.patch<{ message: string }>('/users/current-user/password', payload);
+    return data;
+  } catch (err) {
+    if (isAxiosError(err)) {
+      if (err.code === 'ERR_BAD_REQUEST' && err.response) {
+        if (err.status === 400) {
+          throw new Error(err.response?.data.detail);
+        }
+        if (err.status === 422) {
+          throw new Error("Passwords don't match");
+        }
+      }
+    }
+
+    throw err;
+  }
 };
--- src/features/settings/components/update-password-form.tsx
+++ src/features/settings/components/update-password-form.tsx
@@ -1,9 +1,14 @@
 import * as z from 'zod';
 import { Controller, useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'sonner';
+import { updateCurrentUserPassword } from '@/features/settings/api';
 import { Button } from '@/components/button';
 import { Field, FieldLabel, FieldError } from '@/components/fields';
 import { Input } from '@/components/input';
+import { Alert } from '@/components/alert';
+import { Spinner } from '@/components/spinner';
 
 const UpdatePasswordFormSchema = z
   .object({
@@ -16,7 +21,7 @@ const UpdatePasswordFormSchema = z
     path: ['newConfirmPassword'],
   });
 
-type UpdatePasswordPayload = z.infer<typeof UpdatePasswordFormSchema>;
+export type UpdatePasswordPayload = z.infer<typeof UpdatePasswordFormSchema>;
 
 export function UpdatePasswordForm() {
   const form = useForm<UpdatePasswordPayload>({
@@ -28,25 +33,44 @@ export function UpdatePasswordForm() {
     },
   });
 
+  const {
+    isPending,
+    mutate: updatePassword,
+    error,
+    reset,
+  } = useMutation({
+    mutationFn: updateCurrentUserPassword,
+    onSuccess: ({ message }) => {
+      form.reset({
+        password: '',
+        newPassword: '',
+        newConfirmPassword: '',
+      });
+      toast.success(message);
+    },
+  });
+
   function onSubmit(payload: UpdatePasswordPayload) {
-    console.log(payload);
+    updatePassword(payload);
   }
 
   function handleReset() {
+    reset();
     form.reset({ password: '', newPassword: '', newConfirmPassword: '' });
   }
 
   return (
     <div className="space-y-8">
       <p className="text-bunker-900 font-semibold">Password</p>
+      {error && <Alert>{error.message}</Alert>}
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <Controller
           control={form.control}
           name="password"
           render={({ field, fieldState }) => (
-            <Field fieldId={field.name} fieldError={fieldState.error}>
+            <Field fieldId={field.name} fieldError={fieldState.error} className="space-y-1">
               <FieldLabel>Current password</FieldLabel>
-              <Input {...field} type="password" />
+              <Input {...field} type="password" disabled={isPending} />
               <FieldError />
             </Field>
           )}
@@ -55,9 +79,9 @@ export function UpdatePasswordForm() {
           control={form.control}
           name="newPassword"
           render={({ field, fieldState }) => (
-            <Field fieldId={field.name} fieldError={fieldState.error}>
+            <Field fieldId={field.name} fieldError={fieldState.error} className="space-y-1">
               <FieldLabel>New password</FieldLabel>
-              <Input {...field} type="password" />
+              <Input {...field} type="password" disabled={isPending} />
               <FieldError />
             </Field>
           )}
@@ -66,19 +90,20 @@ export function UpdatePasswordForm() {
           control={form.control}
           name="newConfirmPassword"
           render={({ field, fieldState }) => (
-            <Field fieldId={field.name} fieldError={fieldState.error}>
+            <Field fieldId={field.name} fieldError={fieldState.error} className="space-y-1">
               <FieldLabel>Confirm new password</FieldLabel>
-              <Input {...field} type="password" />
+              <Input {...field} type="password" disabled={isPending} />
               <FieldError />
             </Field>
           )}
         />
         <div className="flex justify-end gap-x-4">
-          <Button type="reset" variant="ghost" onClick={handleReset}>
+          <Button type="reset" variant="ghost" disabled={isPending} onClick={handleReset}>
             Reset
           </Button>
-          <Button type="submit" variant="primary">
-            Update
+          <Button type="submit" variant="primary" disabled={isPending}>
+            {isPending && <Spinner />}
+            {isPending ? 'Updating...' : 'Update'}
           </Button>
         </div>
       </form>
